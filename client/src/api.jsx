2bd9aa47f7d7fe895c8a5ab23b23a11eeecd2839
Sreya <92930185+Sreyareddie@users.noreply.github.com>
2025-01-27 00:18:57 +0530

    Update api.jsx

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL = process.env.REACT_APP_API_URL ? "https://task-management-system-backend-19oc.onrender.com/api":"http://localhost:4000/api";
+const API_BASE_URL = "https://task-management-system-backend-19oc.onrender.com/api";
 
 const API = axios.create({
   baseURL: `${API_BASE_URL}/api`,
