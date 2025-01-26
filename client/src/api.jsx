4b5185ce768667b68dd13716f7e035cd2d6c00a6
Sreya <92930185+Sreyareddie@users.noreply.github.com>
2025-01-27 01:16:54 +0530

    Update api.jsx

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-management-system-backend-19oc.onrender.com/api",
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
 });
 
 API.interceptors.request.use((req) => {
