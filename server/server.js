06077a9395a803cbb5cf4ee62e3d2e3b97d18cba
Sreya <92930185+Sreyareddie@users.noreply.github.com>
2025-01-26 23:17:17 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ import connectDB from "./src/config/mongodb.js";
 import connectCloudinary from "./src/config/cloudinary.js";
 import userRouter from "./src/routes/userRouter.js";
 import feedRouter from "./src/routes/feedRouter.js";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 4000;
+const _dirname=path.resolve();
 
 connectDB();
 connectCloudinary();
@@ -20,6 +22,9 @@ app.use(cors());
 app.use("/api/task", taskRouter);
 app.use("/api/feed", feedRouter);
 app.use("/api/user", userRouter);
+
+app.use(express.static(path.join(_dirname,"/client/dist")));
+app.get("*",(req,res)=>{res.sendFile(path.resolve(_dirname,"client","dist","index.html")});
 
 app.get("/", (req, res) => res.send("API working"));
 
