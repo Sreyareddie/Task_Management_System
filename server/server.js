d6423e391f8d4c903ddced9f090e87e4cec3db15
Sreya <92930185+Sreyareddie@users.noreply.github.com>
2025-01-26 22:16:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,6 +14,15 @@ connectDB();
 connectCloudinary();
 
 app.use(express.json());
+const path = require("path");
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "build")));
+
+// Handle all unmatched routes by serving index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 
 app.use(cors());
