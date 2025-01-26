a5a5cd01b20ab5ebf8198421c2f3f6b6cd91a358
Sreya <92930185+Sreyareddie@users.noreply.github.com>
2025-01-26 19:58:24 +0530

    Update api.jsx

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-management-system-7xhm.vercel.app/",
+  baseURL: "https://task-management-system-7xhm.vercel.app/api",
 });
 
 API.interceptors.request.use((req) => {
